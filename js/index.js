5e38edaf45d466458171c0e86bd2ebfe45d71221
Siyabonga Samson Lukhele <samsonlukhele76@gmail.com>
2024-08-02 21:24:59 +0200

    Add some easier to mantain stuff for the header and Home section

--- js/index.js
+++ js/index.js
@@ -1,26 +1,27 @@
 document.addEventListener("DOMContentLoaded", () => {
-  const hamburger = document.querySelector(".hamburger");
-  const navLinksMobile = document.querySelector(".nav-links-mobile");
+  const menuToggle = document.getElementById("menu-toggle");
+  const navMobile = document.getElementById("nav-mobile");
 
-  hamburger.addEventListener("click", () => {
-    navLinksMobile.classList.toggle("active");
-    hamburger.classList.toggle("active");
+  menuToggle.addEventListener("click", () => {
+    menuToggle.classList.toggle("open");
+    navMobile.classList.toggle("active");
   });
 });
 
-document.addEventListener("DOMContentLoaded", function () {
-  var typed = new Typed("#typed-text", {
+document.addEventListener("DOMContentLoaded", () => {
+  let options = {
     strings: [
-      "An Aspiring Full Stack Dev.",
-      "A Passionate Coder.",
-      "A Basketball enthusiast.",
-      "A Learner.",
+      "Web Development.",
+      "DevOps.",
+      "Databases.",
+      "Basketball.",
+      "Learning.",
     ],
     typeSpeed: 50, // Speed in milliseconds
-    backSpeed: 25, // Speed in milliseconds
-    backDelay: 1000, // Delay before starting backspacing
-    startDelay: 500, // Delay before starting typing
-    loop: true, // Loop the animation
-    showCursor: false, // Show blinking cursor
-  });
+    backSpeed: 25, // Speed of backspacing
+    backDelay: 1000, // Delay before starting to backspace
+    startDelay: 500, // Delay before typing starts
+    loop: true, // Loop indefinitely
+  };
+  new Typed("#typed-text", options); // Correct selector
 });
